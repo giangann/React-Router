89aa23e871956bde4477d95e201cb944e778815a
giangann <gta007md@gmail.com>
2021-12-16 10:34:15 +0700

    delete_row_example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { uuid } from 'uuidv4';
 
-function App() {
+const App = ()=> {
+  const group = {
+    name: 'Group 1',
+    member: [
+      {
+        name:'An',
+        mssv: 20195946
+      },
+      {
+        name:'Minh Anh',
+        mssv: 20195947
+      }
+    ]
+  }
+
+  const handleClick =(e)=> {
+    console.log(e.target.innerHTML);
+    console.log(typeof(e.target));
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderPart handleClick = {handleClick} name = {group.name}/>
+      <ContentPart member = {group.member}/>
     </div>
   );
 }
 
+const HeaderPart = props =>{
+  return(
+    <div>
+      <p onClick={() => {console.log("hello")}}> {props.name}</p>
+      <input type="checkbox" id="item1" name="item" value="Paneer" onClick={props.handleClick}/>
+    </div>
+    
+  )
+}
+
+const ContentPart = props=>{
+  return (
+    <div>
+      {props.member.map(member => 
+        <DetailPart key = {uuid()} index = {uuid()} item = {member}/>
+      )}
+    </div>
+
+  )
+}
+const DetailPart = props =>{
+  return(
+    <div data-index = {props.index}>
+      <p>{props.item.name} - {props.item.mssv} - {props.key}</p>
+      <button onClick={e => e.target.closest(`[data-index="${props.index}"]`).remove()}>Delete</button>
+    </div>
+    
+  )
+}
 export default App;
