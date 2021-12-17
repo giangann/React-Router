2aca4560f5f12860393f7adb266a91438321bb74
giangann <gta007md@gmail.com>
2021-12-17 23:35:06 +0700

    try_to_update_nested_state_properties_with_state_Hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import './App.css';
 import { uuid } from 'uuidv4';
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {status: true}
-  }
+const App=()=>{
+  const [count,setCount] = useState({number:{flag:0}})
 
-  handleOnOff =()=>{
-    this.setState({status: !this.state.status})
-  } 
-
-  //nếu để dạng bên dưới thì sẽ không chạy, (phải để function có dạng callback)
-  // mã lỗi: TypeError: Cannot read properties of undefined (reading 'setState')
-
-  // handleOnOff (){
-  //   this.setState({status: !this.state.status})
-  // } 
-
-  
-
-  render(){
-    return(
-      <div>
-        <button onClick={this.handleOnOff}>{this.state.status?"ON":"OFF"}</button>
-      </div>
+  const handleAdd = ()=>{
+    setCount(
+      count =>(
+        {
+          ...count,
+          number: {
+            ...count.number,
+            flag: count.number.flag+1
+          }
+          }
+      )
     )
   }
+
+  
+  return(
+    <div>
+      <button onClick={handleAdd}>Add</button>
+      {count.number.flag}
+    </div>
+  )
 }
 export default App;
