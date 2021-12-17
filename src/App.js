75fd30c162f071f75f8272e5656f3852d422dcf7
giangann <gta007md@gmail.com>
2021-12-17 09:38:58 +0700

    OnOff_using_state_doByMyself

--- src/App.js
+++ src/App.js
@@ -3,65 +3,21 @@ import { uuid } from 'uuidv4';
 import React from 'react';
 
 class App extends React.Component {
-  group = {
-    name: 'Group 1',
-    member: [
-      {
-        name:'An',
-        mssv: 20195946
-      },
-      {
-        name:'Minh Anh',
-        mssv: 20195947
-      }
-    ]
+  constructor(props){
+    super(props)
+    this.state = {status: true}
+  }
+
+  handleOnOff (){
+    this.setState(this.status = !this.state.status)
   }
 
   render(){
     return(
       <div>
-        <HeaderPart handleClick = {this.handleClick} name = {this.group.name}/>
-        <ContentPart member = {this.group.member}/>
-    </div>
-    )
-    
-  };
-}
-
-class HeaderPart extends React.Component{
-  render(){
-    return(
-      <div>
-        <p > {this.props.name}</p>
-        <input type="checkbox" id="item1" name="item" value="Paneer" onClick={this.props.handleClick}/>
-      </div>
-      
-    )
-  }
-  
-}
-
-class ContentPart extends React.Component{
-  render(){
-    return (
-      <div>
-        {this.props.member.map(member => 
-          <DetailPart key = {uuid()} index = {uuid()} item = {member}/>
-        )}
+        <button onClick={this.handleOnOff}>{this.state.status?"ON":"OFF"}</button>
       </div>
     )
   }
-}
-class DetailPart extends React.Component{
-  render(){
-    return(
-      <div data-index = {this.props.index}>
-        <p>{this.props.item.name} - {this.props.item.mssv} - {this.props.key}</p>
-        <button onClick={e => e.target.closest(`[data-index="${this.props.index}"]`).remove()}>Delete</button>
-      </div>
-      
-    )
-  }
-  
 }
 export default App;
