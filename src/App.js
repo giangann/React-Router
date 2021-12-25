7dedbe92fe9fa75fc5ddce5041525c0b7540ecbd
giangann <gta007md@gmail.com>
2021-12-25 15:11:09 +0700

    add feature: Check when finish Task
    notice thing when write function to change state .isCompleteed (missing ... operator)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import "./App.css";
-// import { uuid } from 'uuidv4';
 import React, { useState } from "react";
 import FormInput from "./Components/FormInput";
 import ShowList from "./Components/ShowList";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
 
 const App = () => {
   // state to save work need to do
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       work: "Brush Teeth",
       deadline: "2021-12-18",
       isCompleted: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       work: "Learn Japaneses",
       deadline: "2021-12-17",
       isCompleted: false,
-
     },
     {
-      id: 3,
+      id: uuidv4(),
       work: "Practice React",
       deadline: "2021-12-21",
       isCompleted: false,
-
     },
   ]);
 
+  // partition data:
+  const partition = _.partition(todos, "isCompleted");
+
   // sort todos data:
-  const todoSorted = todos.sort((a, b) => (a.deadline > b.deadline) ? 1 : -1)
+  const taskCompleted = _.orderBy(partition[0], ["deadline"], ["asc"]);
+  const taskNotCompleted = _.orderBy(partition[1], ["deadline"], ["asc"]);
 
   // funtion delete specifies work
   const handleDelete = (e, index) => {
@@ -39,24 +43,57 @@ const App = () => {
 
   // function handle when add work
   const handleAdd = (value, deadline) => {
-    
     setTodos([
       ...todos,
       {
-        id: Math.floor(Math.random() * 1000),
+        id: uuidv4(),
         work: value,
         deadline: deadline,
       },
     ]);
-    console.log(todos);
-    console.log(todos[0].work);
-    console.log(todos[0].id);
+    
   };
+
+  // function handle when check
+  const handleCheck = (id, value) => {
+    const tempTodos = todos.map((todo) =>
+      todo.id === id ? {...todo, isCompleted: value } : todo
+    );
+    setTodos(tempTodos);
+  };
+
+  console.log(todos)
+  console.log("task completed",taskCompleted)
+  console.log("task not completed",taskNotCompleted)
 
   return (
     <div>
       <FormInput handleAdd={handleAdd} />
-      <ShowList todos={todoSorted} handleDelete={handleDelete} />
+      <div>
+        <h1>Task Completed</h1>
+        {taskCompleted.map((item) => (
+          <ShowList
+            key={item.id}
+            index={item.id}
+            todos={item}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+
+      <div>
+        <h1>Task Not Completed</h1>
+        {taskNotCompleted.map((item) => (
+          <ShowList
+            key={item.id}
+            index={item.id}
+            todos={item}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/ShowList.js
+++ src/Components/ShowList.js
@@ -1,29 +1,18 @@
-import Checkbox from 'antd/lib/checkbox/Checkbox'
-import { useEffect } from 'react'
-import '../App.js'
+import { Button } from "antd";
+import Checkbox from "antd/lib/checkbox/Checkbox";
+import "../App.js";
 
-const ShowList = ({todos, handleDelete}) =>{
-    return(
-        <div>
-            {todos.map(
-                todo => <ShowListDetails todo = {todo} key={todo.id} index = {todo.id} handleDelete = {handleDelete}/>
-            )}
-        </div>
-    )
+const ShowList = ({ index, todos, handleDelete, handleCheck }) => {
+  return (
+    <div data-index={index} key={index}>
+      &lt;{todos.id}&gt;   {todos.work} - {todos.deadline}
+      <Checkbox
+        defaultChecked={todos.isCompleted}
+        onChange={e => (handleCheck(todos.id, e.target.checked))}
+      ></Checkbox>
+      <Button onClick={(e) => handleDelete(e, index)}>Delete</Button>
+    </div>
+  );
+};
 
-}
-
-const ShowListDetails = ({todo,index,handleDelete}) =>{
-    const chooseFav = (e,index) =>{
-        // console.log("favourite choose")
-        console.log(e.target(`[data-index="${index}"]`).value)
-    }
-    return(
-        <div data-index = {todo.index}>
-            {todo.work}-{todo.deadline}
-            <button type='submit' onClick={(e) => handleDelete(e,index)}>Delete</button>
-            <Checkbox onChange={chooseFav}></Checkbox>
-        </div>
-    )
-}
 export default ShowList;
